0be47869e02331172a30c6047497613014f827aa
lucas <lucasdsf7@gmail.com>
2026-04-22 22:48:43 -0300

    There is no spoon ep2 implementing dfs

--- There-Is-No-Spoon/Episode-2/solver.go
+++ There-Is-No-Spoon/Episode-2/solver.go
@@ -1,23 +1,48 @@
+/*
+https://www.conceptispuzzles.com/index.aspx?uri=puzzle%2Fhashi%2Ftechniques
+*/
+
 package main
 
 import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
+	"strings"
+	"time"
 )
 
 var Commands []string
+var Visited = make(map[string]struct{}, 10000)
+
+type Move struct {
+	From   *Node
+	To     *Node
+	Amount int
+}
 
 type Node struct {
 	Y         int
 	X         int
 	Amount    int
 	Neighbors []*Node
+	Bridges   map[*Node]int
 }
 
 func (n *Node) String() string {
 	return fmt.Sprintf("Y: %d X: %d Links: %d Nº Neighbors: %v\n", n.X, n.Y, n.Amount, len(n.Neighbors))
+}
+
+func (n *Node) NumberNeighbors() int {
+	counter := 0
+	for _, node := range n.Neighbors {
+		if node.Amount > 0 {
+			counter += 1
+		}
+	}
+	return counter
 }
 
 func FindNeighbors(nodesMap map[int][]*Node) {
@@ -31,16 +56,144 @@ func FindNeighbors(nodesMap map[int][]*Node) {
 	}
 }
 
-func TrivialSolutions(nodes []*Node) {
-	for _, node := range nodes {
-		if len(node.Neighbors) != 1 {
-			continue
-		}
-		neighbor := node.Neighbors[0]
-		Commands = append(Commands, fmt.Sprintf("%d %d %d %d %d", node.X, node.Y, neighbor.X, neighbor.Y, node.Amount))
-		neighbor.Amount -= node.Amount
-		node.Amount = 0
+func Link(move Move) {
+	n := move.From
+	node := move.To
+	Commands = append(Commands, fmt.Sprintf("%d %d %d %d %d", n.X, n.Y, node.X, node.Y, move.Amount))
+	node.Amount -= move.Amount
+	n.Amount -= move.Amount
+	node.Bridges[n] += move.Amount
+	n.Bridges[node] += move.Amount
+}
+
+func UnLink(moves []Move) {
+	for _, move := range moves {
+		node := move.From
+		n := move.To
+		node.Amount += move.Amount
+		n.Amount += move.Amount
+		node.Bridges[n] -= move.Amount
+		n.Bridges[node] -= move.Amount
 	}
+	Commands = Commands[:len(Commands)-len(moves)]
+}
+
+func TrivialSolutions(nodes []*Node, turn int) []Move {
+	moves := []Move{}
+	for {
+		links := false
+		for _, node := range nodes {
+			if node.Amount == 0 || node.NumberNeighbors() == 0 {
+				continue
+			}
+			if node.Amount/node.NumberNeighbors() == 2 {
+				for _, neighbor := range node.Neighbors {
+					if neighbor.Amount == 0 || node.Bridges[neighbor] != 0 {
+						continue
+					}
+					links = true
+					moves = append(moves, Move{node, neighbor, 2})
+					Link(moves[len(moves)-1])
+				}
+			}
+			if (node.Amount == 1 || node.Amount == 2) && node.NumberNeighbors() == 1 {
+				for _, neighbor := range node.Neighbors {
+					if neighbor.Amount == 0 || node.Bridges[neighbor]+node.Amount > 2 {
+						continue
+					}
+					links = true
+					moves = append(moves, Move{node, neighbor, node.Amount})
+					Link(moves[len(moves)-1])
+				}
+			}
+			if node.Amount == 3 && node.NumberNeighbors() == 2 {
+				for _, neighbor := range node.Neighbors {
+					if neighbor.Amount == 0 || node.Bridges[neighbor] > 1 {
+						continue
+					}
+					links = true
+					moves = append(moves, Move{node, neighbor, 1})
+					Link(moves[len(moves)-1])
+				}
+			}
+			if node.Amount == 5 && node.NumberNeighbors() == 3 {
+				for _, neighbor := range node.Neighbors {
+					if neighbor.Amount == 0 || node.Bridges[neighbor] > 1 {
+						continue
+					}
+					links = true
+					moves = append(moves, Move{node, neighbor, 1})
+					Link(moves[len(moves)-1])
+				}
+			}
+			if node.Amount == 7 && node.NumberNeighbors() == 4 {
+				for _, neighbor := range node.Neighbors {
+					if neighbor.Amount == 0 || node.Bridges[neighbor] > 1 {
+						continue
+					}
+					links = true
+					moves = append(moves, Move{node, neighbor, 1})
+					Link(moves[len(moves)-1])
+				}
+			}
+		}
+		if !links {
+			break
+		}
+	}
+	return moves
+}
+
+func CheckSolution(nodes []*Node) bool {
+	solveds := 0
+	for _, node := range nodes {
+		if node.Amount == 0 {
+			solveds += 1
+		}
+	}
+	return solveds == len(nodes)
+}
+
+func GetState(nodes []*Node) string {
+	str := strings.Builder{}
+	for _, node := range nodes {
+		fmt.Fprintf(&str, "%d%d%d", node.X, node.Y, node.Amount)
+		for _, neighbor := range node.Neighbors {
+			fmt.Fprintf(&str, "%d%d%d", neighbor.X, neighbor.Y, node.Bridges[neighbor])
+		}
+	}
+	return str.String()
+}
+
+func DFS(nodes []*Node, turn int) bool {
+	if CheckSolution(nodes) {
+		return true
+	}
+	stateKey := GetState(nodes)
+	if _, seen := Visited[stateKey]; seen {
+		return false
+	}
+	for _, node := range nodes {
+		for _, neighbor := range node.Neighbors {
+			for _, amount := range [2]int{2, 1} {
+				if node.Amount < amount || neighbor.Amount < amount {
+					continue
+				}
+				if node.Bridges[neighbor]+amount > 2 {
+					continue
+				}
+				move := Move{node, neighbor, amount}
+				Link(move)
+				moves := append(TrivialSolutions(nodes, turn), move)
+				if DFS(nodes, turn+1) {
+					return true
+				}
+				UnLink(moves)
+			}
+		}
+	}
+	Visited[stateKey] = struct{}{}
+	return false
 }
 
 func main() {
@@ -70,20 +223,35 @@ func main() {
 				continue
 			}
 			amount, _ := strconv.Atoi(string(node))
-			newNode := &Node{y, x, amount, []*Node{}}
+			newNode := &Node{y, x, amount, []*Node{}, map[*Node]int{}}
 			Nodes = append(Nodes, newNode)
 			Ynodes[y] = append(Ynodes[y], newNode)
 			Xnodes[x] = append(Xnodes[x], newNode)
 			// Ynodes and Xnodes are sorted by default.
 		}
 	}
+	start := time.Now()
 	FindNeighbors(Xnodes)
 	FindNeighbors(Ynodes)
-	fmt.Println(Nodes)
-	TrivialSolutions(Nodes)
-	fmt.Println(Nodes)
+	// TrivialSolutions(Nodes, 0)
+	fmt.Fprintln(os.Stderr, time.Since(start))
+	sort.Slice(Nodes, func(i, j int) bool {
+		return Nodes[i].Amount < Nodes[j].Amount
+	})
+	fmt.Fprintln(os.Stderr, Nodes)
+	DFS(Nodes, 1)
+	fmt.Fprintln(os.Stderr, time.Since(start))
+	fmt.Fprintln(os.Stderr, Nodes)
 	for _, cmd := range Commands {
 		fmt.Println(cmd)
+	}
+	for _, node := range Nodes {
+		fmt.Println(node)
+		fmt.Println("Bridges:")
+		for k, bridge := range node.Bridges {
+			fmt.Printf("Nº: %d Node: %v", bridge, k)
+		}
+		fmt.Println()
 	}
 	// fmt.Fprintln(os.Stderr, "Debug messages...")
 }
